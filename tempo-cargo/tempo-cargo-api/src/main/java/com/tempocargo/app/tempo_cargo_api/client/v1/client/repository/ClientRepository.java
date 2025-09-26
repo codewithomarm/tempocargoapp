617dbab3939a626e1a2053a1329b6c995bd301a1
codewithomarm <codewithomarm@gmail.com>
2025-09-26 05:58:58 -0500

    Added getNextPoBoxNumber to implement RegisterService

--- tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/client/v1/client/repository/ClientRepository.java
+++ tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/client/v1/client/repository/ClientRepository.java
@@ -2,7 +2,10 @@ package com.tempocargo.app.tempo_cargo_api.client.v1.client.repository;
 
 import com.tempocargo.app.tempo_cargo_api.client.v1.client.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
+    @Query(value = "SELECT nextval('client.po_box_seq')", nativeQuery = true)
+    Long getNextPoBoxNumber();
 }
