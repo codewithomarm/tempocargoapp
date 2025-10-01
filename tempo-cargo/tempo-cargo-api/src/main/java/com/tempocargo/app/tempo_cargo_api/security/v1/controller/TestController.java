8cae288b8c2cf2541b315c0c22c3a7b2666c0396
codewithomarm <codewithomarm@gmail.com>
2025-10-01 02:15:39 -0500

    Added endpoint for sending test email

--- tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/controller/TestController.java
+++ tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.tempocargo.app.tempo_cargo_api.security.v1.controller;
 
+import com.tempocargo.app.tempo_cargo_api.security.v1.service.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
 
+    private final MailService mailService;
+
     @GetMapping("/protected-hello")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Hello from Protected");
+    }
+
+    @GetMapping("/send-mail")
+    public ResponseEntity<String> sendTestMail() {
+        mailService.sendTestMail("codewithomarm@gmail.com");
+        return ResponseEntity.ok("Test email sent successfully to codewithomarm@gmail.com!");
     }
 }
