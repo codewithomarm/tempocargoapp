74b7c5b448b7b5660c108a164ce2b0949efe48e3
codewithomarm <codewithomarm@gmail.com>
2025-10-02 15:18:17 -0500

    Removed docker-mailserver arquitecture and replaced it with SendGrid gradle dependency

--- tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/service/MailService.java
+++ tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/service/MailService.java
@@ -1,22 +1,39 @@
 package com.tempocargo.app.tempo_cargo_api.security.v1.service;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class MailService {
 
-    private final JavaMailSender mailSender;
+    @Value("${sendgrid.api-key}")
+    private String sendgridApiKey;
 
-    public void sendTestMail(String to) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("no-reply@tempocargo.lat");
-        message.setTo(to);
-        message.setSubject("Test Email from TempoCargo API");
-        message.setText("Hello! This is a test email sent from your API and Docker Mailserver setup.");
-        mailSender.send(message);
+    public void sendTestMail(String toEmail) throws IOException {
+        Email from = new Email("no-reply@tempocargo.lat");
+        String subject = "Bienvenido a TempoCargo!";
+        Email to = new Email(toEmail);
+        Content content = new Content("text/plain", "Gracias por registrarte en TempoCargo.");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        Response response = sg.api(request);
+        System.out.println("SendGrid Status: " + response.getStatusCode());
     }
 }
