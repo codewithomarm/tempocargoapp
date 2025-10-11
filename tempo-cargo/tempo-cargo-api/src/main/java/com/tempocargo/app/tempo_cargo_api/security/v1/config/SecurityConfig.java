877acba722ce8ce7e389634ec56cbca0c1649ecf
codewithomarm <codewithomarm@gmail.com>
2025-10-11 14:25:05 -0500

    Added registration endpoints to public access

--- tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/config/SecurityConfig.java
+++ tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/login",
                                 "/api/v1/auth/refresh",
-                                "/api/v1/auth/register")
+                                "/api/v1/auth/registration",
+                                "/api/v1/auth/registration/*")
                         .permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/logout").authenticated()
                         .anyRequest().authenticated()
