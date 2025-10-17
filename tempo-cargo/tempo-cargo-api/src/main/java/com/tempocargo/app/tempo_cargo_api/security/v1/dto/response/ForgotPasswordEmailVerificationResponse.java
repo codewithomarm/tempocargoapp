a7b603461a8ab2e9a7703afa504462ca982a52b3
codewithomarm <codewithomarm@gmail.com>
2025-10-17 09:27:05 -0500

    Added response DTOs for forgetpassword workflow

---
+++ tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/dto/response/ForgotPasswordEmailVerificationResponse.java
@@ -0,0 +1,5 @@
+package com.tempocargo.app.tempo_cargo_api.security.v1.dto.response;
+
+public record ForgotPasswordEmailVerificationResponse(
+        String message
+) {}
---
+++ tempo-cargo/tempo-cargo-api/src/main/java/com/tempocargo/app/tempo_cargo_api/security/v1/dto/response/ResetPasswordResponse.java
@@ -0,0 +1,5 @@
+package com.tempocargo.app.tempo_cargo_api.security.v1.dto.response;
+
+public record ResetPasswordResponse(
+        String message
+) {}
